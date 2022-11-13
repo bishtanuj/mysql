2677fad5e6c534198c21b035a376806bb5857850
ANUJ BISHT <anujbisht09@gmail.com>
2022-11-13 21:10:37 +0530

    Update insert_values.sql

--- INSERT VALUES/insert_values.sql
+++ INSERT VALUES/insert_values.sql
@@ -1,2 +1,4 @@
 insert into employee(Eno, Ename, Job, Sal, DepNo, Mgr, HireDate)
-values(7369, 'SMITH', 'CLERK', 800, 20, 7788, '2012-10-09');
+values(7369, 'SMITH', 'CLERK', 800, 20, 7788, '2012-10-09'),
+(7364, 'ALLEN', 'SALESMAN', 950, 30, 7782, '2011-09-08'),
+(7563, 'WARD', 'SALESMAN', 1100, 30, 7563, '2018-12-05');
